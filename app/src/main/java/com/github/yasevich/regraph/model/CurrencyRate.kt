5dbe4cd25d62c7aa9eb2d9ceaeea9bd3b2f7f0a1
Slava Yasevich <yasevich.slava@gmail.com>
2018-01-23 23:04:54 +0300

    Add rebase method to CurrencyRates

--- app/src/main/java/com/github/yasevich/regraph/model/CurrencyRate.kt
+++ app/src/main/java/com/github/yasevich/regraph/model/CurrencyRate.kt
@@ -2,7 +2,7 @@ package com.github.yasevich.regraph.model
 
 import java.math.BigDecimal
 
-data class CurrencyRate(val currencyCode: String, private val amount: BigDecimal, private val timestamp: Long) {
+data class CurrencyRate(val currencyCode: String, val amount: BigDecimal, private val timestamp: Long) {
     val point: Point
         get() = Point(timestamp.toDouble(), amount.toDouble())
 }
--- app/src/main/java/com/github/yasevich/regraph/model/CurrencyRates.kt
+++ app/src/main/java/com/github/yasevich/regraph/model/CurrencyRates.kt
@@ -1,3 +1,19 @@
 package com.github.yasevich.regraph.model
 
-class CurrencyRates(val rates: List<CurrencyRate> = emptyList(), val timestamp: Long)
+import java.math.BigDecimal
+
+data class CurrencyRates(val rates: List<CurrencyRate> = emptyList(), val timestamp: Long) {
+
+    fun rebase(currencyCode: String): CurrencyRates {
+        if (rates.isEmpty() || rates.first().currencyCode == currencyCode) {
+            return this
+        }
+
+        val base = rates.find { it.currencyCode == currencyCode }
+                ?: throw IllegalArgumentException("no such currency code: $currencyCode")
+        val result = mutableListOf(base.copy(amount = BigDecimal.ONE))
+        rates.filter { it != base }
+                .forEach { result.add(it.copy(amount = it.amount.divide(base.amount, BigDecimal.ROUND_HALF_UP))) }
+        return copy(rates = result)
+    }
+}
